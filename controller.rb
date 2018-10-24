b030d68d04c256f2a8908ec06a7a075a1cc03998
StevieCode <sdcowan@hotmail.com>
2018-10-24 15:55:12 +0100

    send to Rab

---
+++ controller.rb
@@ -0,0 +1,33 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./models/house')
+require_relative('./models/student')
+
+
+get "/" do
+  @students = Student.all()
+  erb(:home)
+end
+
+
+get "/student" do
+  @students = Student.all()
+  erb(:index)
+end
+
+get "/student/new" do
+  erb(:new)
+end
+
+get "/student/:id" do
+   @student = Student.find(params[:id])
+   @student.save()
+  erb(:show)
+end
+
+post "/student" do
+   @student = Student.new(params)
+   @student.save()
+  redirect "/student"
+end
